b376167f05077388b03e279b1fc1b298489e0169
Laura <lauraof@al.insper.edu.br>
2025-05-01 02:27:29 -0300

    guardar_dado

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,14 @@ def rolar_dados(n):
         dados.append(dado)
         i += 1
     return dados
+
+def guardar_dado(dados_rolados, dados_guardados, dado_p_guardar):
+    novos_dados_rolados = []
+    i = 0
+    while i < len(dados_rolados):
+        if i == dado_p_guardar:
+            dados_guardados.append(dados_rolados[i])
+        else:
+            novos_dados_rolados.append(dados_rolados[i])
+        i += 1
+    return [novos_dados_rolados, dados_guardados]
